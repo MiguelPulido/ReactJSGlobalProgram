a8ac04d62b94348c3e17c64054ae24fd48194caa
Miguel Pulido <ingenieromiguelpulido@gmail.com>
2025-08-20 17:42:45 -0500

    Initial test configuration

---
+++ jest.setup.cjs
@@ -0,0 +1,32 @@
+require('@testing-library/jest-dom');
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (
+      typeof args[0] === 'string' &&
+      args[0].includes('Warning: ReactDOMTestUtils.act')
+    ) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
